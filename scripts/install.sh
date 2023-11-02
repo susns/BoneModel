7b669a205f1758dfc48891a3ab9103ed7566615e
user823 <77144738+user823@users.noreply.github.com>
2023-11-01 21:50:41 -0500

    Update install.sh
    install.sh has tested

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,15 +2,18 @@
 
 opengr_url='https://github.com/STORM-IRIT/OpenGR.git'
 opengr_dir='./OpenGR'
-git clone ${opengr_url}
+if [[ ! -a ${opengr_dir} ]]; then 
+  git clone ${opengr_url}
+fi
+
 if [[ -d ${opengr_dir} ]] && cd ${opengr_dir}; then
   mkdir build && cd build
   cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./install
   make install
-  cd install
-  ./scripts/run-example.sh
+  cd install/scripts
+  ./run-example.sh
   echo "Congratulations for install finished!"
-  export PATH=$PATH:$(PWD)/bin
+  export PATH=$PATH:$(pwd)/bin
 else
   echo "can't find OpenGR dir, something error."
 fi
