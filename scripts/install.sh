2d797809ace3b7f127bd949576202714291a45a4
user823 <77144738+user823@users.noreply.github.com>
2023-11-08 02:29:05 -0600

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,13 +7,13 @@ if [[ ! -a ${opengr_dir} ]]; then
 fi
 
 if [[ -d ${opengr_dir} ]] && cd ${opengr_dir}; then
-  mkdir build && cd build
+  mkdir build && cd build || exit
   cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./install
   make install
-  cd install/scripts
+  cd install/scripts || exit
   ./run-example.sh
   echo "Congratulations for install finished!"
-  export PATH=$PATH:$(pwd)/bin
+  export PATH=$(pwd)/bin:${PATH}
 else
   echo "can't find OpenGR dir, something error."
 fi
